29c8c7e7c698d5b3c6550221fde28c1d5ff9ee03
open-codebranch <79106441+open-codebranch@users.noreply.github.com>
2021-02-16 19:02:48 +0600

    another

---
+++ psd-html4-systematic-c/resources/js/main.js
@@ -0,0 +1,20 @@
+// $(document).ready(function()
+// {
+    // BX SLIDER===========================
+//     $(".slider").bxSlider();
+// });
+
+
+$(document).ready(function(){
+    $('.slider').bxSlider();
+
+    $('#main-nav').hcOffcanvasNav({
+        disableAt: 1024
+    });
+
+    // $("#main-nav").meanmenu({
+    //     meanMenuContainer: "#mobile_menu",
+    //     meanScreenWidth: 1024
+    // });
+
+});
